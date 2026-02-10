db81beb89204a6b422d61e7236f1c23a1dbf0d12
done778 <57587831+done778@users.noreply.github.com>
2026-02-10 14:28:53 +0900

    [feat] 제네릭 싱글톤 클래스 추가
    싱글톤 패턴 적용할 매니저는 이 클래스를 상속받으면 됩니다.

---
+++ Assets/_Scripts/System/Singletone.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+
+    [SerializeField] private bool _dontDestroyOnLoad = true;
+
+    protected virtual void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this as T;
+
+        if (_dontDestroyOnLoad)
+            DontDestroyOnLoad(gameObject);
+
+        OnSingletonAwake();
+    }
+
+    protected virtual void OnSingletonAwake() { }
+}
+
