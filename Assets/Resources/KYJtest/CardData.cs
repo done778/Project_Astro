4383560745e85446a3f32fe69e46dbd33b865442
Dokkaeb <yj4705@gmail.com>
2026-02-11 14:24:05 +0900

    [feat] 맵 디자인 크기조정, 테스트용 코드 추가

---
+++ Assets/Resources/KYJtest/CardData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardData", menuName = "Scriptable Objects/CardData")]
+public class CardData : ScriptableObject
+{
+    public Sprite heroImg;
+    public GameObject heroPrefab;
+}
