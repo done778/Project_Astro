d1ec59c1d5fa93a8e9b662801612f9bd4eaf10df
KangMin9704 <wnrkdals123@gmail.com>
2026-02-12 09:42:51 +0900

    [Fix] HeroStatus 데이터 수정
    HeroStatus SO로 임시 작성해둔 부분 ctor을 통해 복제하여 사용하기 좋은 방식으로 수정

--- Assets/_Scripts/Hero/Data/HeroStatus.cs
+++ Assets/_Scripts/Hero/Data/HeroStatus.cs
@@ -1,17 +1,27 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "HeroStatus", menuName = "ScriptableObject/HeroStatus")]
-public class HeroStatus : ScriptableObject
+﻿public class HeroStatus
 {
-    public float Base_Hp;
-    public float Base_Shield;
-    public float Base_Attack_Power;
-    public float Base_Healing_Power;
+    public float Hp;
+    public float Shield;
+    public float Attack_Power;
+    public float Healing_Power;
     public float Min_Attack_Speed;
     public float Max_Attack_Speed;
     public float Min_Move_Speed;
     public float Max_Move_Speed;
     public float Min_Spawn_Cooldown;
     public float Max_Spawn_Cooldown;
-    
+
+    public HeroStatus(HeroStatus origin)
+    {
+        this.Hp = origin.Hp;
+        this.Shield = origin.Shield;
+        this.Attack_Power = origin.Attack_Power;
+        this.Healing_Power = origin.Healing_Power;
+        this.Min_Attack_Speed = origin.Min_Attack_Speed;
+        this.Max_Attack_Speed = origin.Max_Attack_Speed;
+        this.Min_Move_Speed = origin.Min_Move_Speed;
+        this.Max_Move_Speed = origin.Max_Move_Speed;
+        this.Min_Spawn_Cooldown = origin.Min_Spawn_Cooldown;
+        this.Max_Spawn_Cooldown = origin.Max_Spawn_Cooldown;
+    }
 }
