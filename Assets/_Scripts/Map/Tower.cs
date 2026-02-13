3ec0943c7c9cdedd84731d6453bc00f690014f3f
Hyun <oiwer23@gmail.com>
2026-02-13 10:17:33 +0900

    [refactor] 타워 임의로 범위,공격력 추가

--- Assets/_Scripts/Map/Tower.cs
+++ Assets/_Scripts/Map/Tower.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] private Team _team;
-    [SerializeField] private float _hp; 
+    [SerializeField] private float _hp;
     public event Action OnTowerDestroyed;
     private bool _isDestroyed = false;
-    
+
+    //26-02-13 주현중 수정 (임의로 범위,공격력을 설정해서 진행)
+    [SerializeField] private float _damage;
+    [SerializeField] private float _detectRange;
+
 
     public void TakeDamage(float amount)
     {
