978b822a1e9bcf80c680a3e213537138876f6855
firedHouse <y.hyungu4704@gmail.com>
2026-02-13 18:04:17 +0900

    [feat] 어드레서블 태그,그룹 자동화

--- Assets/_Scripts/Editor/CsvPostprocessor.cs
+++ Assets/_Scripts/Editor/CsvPostprocessor.cs
@@ -1,16 +1,66 @@
+using System.IO;
+using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Settings;
 using UnityEngine;
+
+//어드레서블 네임스페이스
+#if UNITY_ADDRESSABLES
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Settings;
+#endif
 
 public class CsvPostprocessor : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    //감시 폴더와 보관할 그룹명
+    private const string CsvFolder = "Assets/_Data/Csv";
+    private const string TargetGroupName = "TableData";
 
-    // Update is called once per frame
-    void Update()
+    //어떤 변화라도 생기면 호출되도록 설정
+    static void OnPostprocessAllAssets(
+        string[] importedAssets,
+        string[] deletedAssets,
+        string[] movedAssets,
+        string[] movedFromAssetPaths)
     {
-        
+        var settings = AddressableAssetSettingsDefaultObject.Settings;
+        if (settings == null) return; //세팅 파일 없으면 무시
+
+        //TableData 그룹 체크 및 없으면 생성
+        AddressableAssetGroup group = settings.FindGroup(TargetGroupName);
+        if (group == null)
+        {
+            group = settings.CreateGroup(TargetGroupName, false, false, true, null);
+        }
+
+        bool isDirty = false;
+
+        //파일검사
+        foreach (string path in importedAssets)
+        {
+            //경로 + 확장자 체크
+            if (path.StartsWith(CsvFolder) && Path.GetExtension(path).ToLower() == ".csv")
+            {
+                //고유 GUID 추출
+                string guid = AssetDatabase.AssetPathToGUID(path);
+
+                //그룹 등록
+                AddressableAssetEntry entry = settings.CreateOrMoveEntry(guid, group);
+                
+                //어드레스 설정 
+                //Assets/_Data/Csv/Hero.csv 에서 Hero로 단축해서 등록
+                string fileName = Path.GetFileNameWithoutExtension(path);
+                entry.address = fileName;
+
+                Debug.Log($"어드레서블 {fileName} 자동 등록 완료");
+                isDirty = true;
+            }
+        }
+        //변경사항 있으면 저장
+        if (isDirty)
+        {
+            EditorUtility.SetDirty(settings);
+            AssetDatabase.SaveAssets();
+        }
     }
 }
