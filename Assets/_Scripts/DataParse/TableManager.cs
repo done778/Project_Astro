c9d89e09f569a0f0f51b28f75b3726ba00d61ec7
firedHouse <y.hyungu4704@gmail.com>
2026-02-15 03:32:00 +0900

    [refactor] TableManager 리팩토링
    02.14 로직 변경 => 모든 테이블을 찾아서 어드레서블로 로드시키도록 변경

--- Assets/_Scripts/DataParse/TableManager.cs
+++ Assets/_Scripts/DataParse/TableManager.cs
@@ -1,22 +1,27 @@
 using System.Reflection;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
+//02.14 로직 변경 => 모든 테이블을 찾아서 어드레서블로 로드시키도록 변경
 public class TableManager : Singleton<TableManager>
 {
     //테이블 목록
     //추가 시 제일 아래에 이어 작성
     public TableBase<ItemData> ItemTable = new TableBase<ItemData>();
-    public TableBase<EffectData> EffectTable = new TableBase<EffectData>();
+    public TableBase<HeroData> HeroTable = new TableBase<HeroData>();
+    public TableBase<ItemEffectData> ItemEffectTable = new TableBase<ItemEffectData>();
 
 
     protected override void Awake()
     {
         base.Awake();
-        LoadAllData();
+        //코루틴으로 변경
+        StartCoroutine(LoadAllData());
     }
 
-    private void LoadAllData()
+
+    private IEnumerator LoadAllData()
     {
         //TableManager의 모든 public 변수를 가져옴
         FieldInfo[] fields = this.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public);
@@ -29,21 +34,26 @@ public class TableManager : Singleton<TableManager>
                 //변수 이름을 가져오기
 
                 //테이블받아오면 다시 수정
-                
-                //2.11 수정, 변수명에서 Table 떼고 파일 찾기
-                string fileName = field.Name.Replace("Table", "");
-                string path = $"Data/{fileName}"; 
 
-                //해당 변수의 인스턴스를 가져옴
+                //2.14 수정, 변수명에서 Table 떼고 어드레서블 주소 만들기
+                string addressKey = field.Name.Replace("Table", "");
+
+                //테이블 인스턴스를 가져옴
                 object tableInstance = field.GetValue(this);
 
-                //Load 함수 실행
-                MethodInfo loadMethod = field.FieldType.GetMethod("Load");
+                //Load => LoadAsync 함수 실행
+                MethodInfo loadMethod = field.FieldType.GetMethod("LoadAsync");
 
                 if (loadMethod != null)
                 {
-                    loadMethod.Invoke(tableInstance, new object[] { path });
-                    Debug.Log($"{fileName} 로드 완료 (경로: {path})");
+                    //LoadAsync 실행 => Item테이블꺼 받고 => 인자값은? Item(주소값) => 인보크한거 코루틴이니 형변환
+                    var loadRoutine = loadMethod.Invoke(tableInstance, new object[] { addressKey }) as IEnumerator;
+
+                    if (loadRoutine != null)
+                    {
+                        //로드가 끝날 때까지 대기
+                        yield return StartCoroutine(loadRoutine);
+                    }
                 }
             }
         }
